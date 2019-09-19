0646784b60df151ff02b54a6dbc914a18eb8db45
Davor <davor.dimoski@hotmail.com>
2019-09-19 05:43:57 +0200

    update code

--- ImageStitching.py
+++ ImageStitching.py
@@ -1,5 +1,3 @@
-# python ImageStitching.py -l C:\Users\dado_\PycharmProjects\ImageStitching\images\3.jpg -r C:\Users\dado_\PycharmProjects\ImageStitching\images\4.jpg
-
 import cv2
 import numpy as np
 import argparse
@@ -29,7 +27,6 @@ def find_good_matches(descriptors, ratio):
     return good
 
 
-'''Defining arguments needed to enter from command line'''
 ap = argparse.ArgumentParser()
 ap.add_argument("-l", "--limage", required=True, help="Path to the left image to form a panorama photo")
 ap.add_argument("-r", "--rimage", required=True, help="Path to the right image to form a panorama photo")
@@ -48,9 +45,6 @@ sift = cv2.xfeatures2d.SIFT_create()
 kp1, des1 = sift.detectAndCompute(img_right_bw, None)
 kp2, des2 = sift.detectAndCompute(img_left_bw, None)
 
-'''cv2.imshow("original image left keypoints", cv2.drawKeypoints(img1, kp1, None))
-cv2.waitKey()'''
-
 # find the overlapping points between the images with Brute Force Matcher
 bfmatcher = cv2.BFMatcher()
 matching_descriptors = bfmatcher.knnMatch(des1, des2, k=2)
@@ -58,11 +52,6 @@ matching_descriptors = bfmatcher.knnMatch(des1, des2, k=2)
 # where each sublist contains k objects'
 
 good_matches = find_good_matches(matching_descriptors, 0.3)
-
-'''draw_params = dict(matchColor=(0, 195, 255), singlePointColor=None, flags=2)
-img3 = cv2.drawMatches(img_right, kp1, img_left, kp2, good_matches, None, **draw_params)
-cv2.imshow("drawing matches", img3)
-cv2.waitKey()'''
 
 '''To compute homography we need at least 4 matches'''
 MIN_MATCH_COUNT = 4
@@ -75,11 +64,9 @@ if len(good_matches) > MIN_MATCH_COUNT:
     height, width = img_right_bw.shape
     pts = np.float32([[0, 0], [0, height - 1], [width - 1, height - 1], [width - 1, 0]]).reshape(-1, 1, 2)
     dst = cv2.perspectiveTransform(pts, M)
-    #img_left_bw = cv2.polylines(img_left_bw, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
     dst = cv2.warpPerspective(img_right, M, (img_left.shape[1] + img_right.shape[1], img_left.shape[0]))
 
     dst[0:img_left.shape[0], 0:img_left.shape[1]] = img_left
-    cv2.imwrite("panorama-untrimmed.png", dst)
     cv2.imshow("final", trim(dst))
     cv2.imwrite("panorama.png", trim(dst))
     cv2.waitKey()
